54d84945ff398d6997463a10f0d3737beea53ebc
Felix Avelar <felixavco@gmail.com>
2019-05-20 23:24:59 -0600

    added Express server

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const next = require('next');
+
+const dev = process.env.NODE_ENV !== 'production';
+const PORT = process.env.PORT || 3000;
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app
+    .prepare()
+    .then(() => {
+        const server = express();
+
+        server.get('*', (req, res) => {
+            return handle(req, res);
+        })
+
+        server.listen(PORT, (err) => {
+            if(err) throw err;
+            console.log("> Server started on port " + PORT);
+        })
+    })
+    .catch(error => {
+        console.error(error.stack);
+        process.exit(1)
+    })
